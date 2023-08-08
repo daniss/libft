ee8ddeace9970099801499a4a8af5143b4066558
daniss <daniscin@gmail.com>
2023-08-08 18:04:56 +0200

    correction

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -7,7 +7,7 @@ int		ft_memcmp(const void *s1, const void *s2, size_t n)
 	i = 0;
 	while (i < n)
 	{
-		if (*(char*)(s1 + i) != *(char*)(s2 + i))
+		if (((char*)s1)[i] != ((char*)s2)[i])
 			return ((int)((char*)(s1 + i) - (char*)(s2 + i)));
 		i++;
 	}
--- ft_strncmp.c
+++ ft_strncmp.c
@@ -16,7 +16,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	while (s1[i] && s2[i] == s1[i] && n >= 1)
+	while (s2[i] == s1[i] && n >= 1)
 	{
 		n--;
 		i++;
